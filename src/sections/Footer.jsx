bf94ef43cc981346da65cc7eec0b8149b0854999
hdmasa <mahsasamie894@gmail.com>
2026-09-13 01:14:35 +0330

    footer section

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,16 @@
+import { mySocials } from "../constants";
+const Footer = () => {
+  return(
+    <section className="flex flext-wrap items-center justify-between gap-5 pb-3 text-sm text-neutral-400 c-space">
+      <div className="flex gap-3">
+        {mySocials.map((social,index) => (
+          <a href={social.href} key={index}>
+            <img src={social.icon} className="w-5 h-5" alt={social.name} />
+          </a>
+        ))}
+      </div>
+    </section>
+  )
+};
+
+export default Footer
